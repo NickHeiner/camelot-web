38660b9270103bb4fe888a9fd0c7cb9338b2d667
Nick Heiner <nth23@cornell.edu>
2017-02-08 12:30:31 -0500

    basic firebase is working!

--- src/App.js
+++ src/App.js
@@ -1,15 +1,41 @@
 import React, { Component } from 'react';
 import { PageHeader, Button } from 'react-bootstrap';
 import './App.less';
+import firebase from 'firebase';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.auth = new firebase.auth();
+    this.provider = new firebase.auth.GoogleAuthProvider();
+    this.state = {
+      currentUser: this.auth.currentUser
+    };
+  }
+
+  componentWillMount() {
+    this.unsubscribeFirebaseAuthWatcher = this.auth.onAuthStateChanged(currentUser => this.setState({currentUser}));
+  }
+
+  componentWillUnmount() {
+    this.unsubscribeFirebaseAuthWatcher();
+  }
+
   render() {
+    const appBody = this.state.currentUser ?
+      (<p>You are signed in</p>) :
+      (<Button bsStyle="primary" onClick={this.signIn.bind(this)}>Sign In</Button>);
+
     return (
       <div className="App">
         <PageHeader>Camelot</PageHeader>
-        <Button bsStyle="primary">Sign In</Button>
+        {appBody}
       </div>
     );
+  }
+
+  signIn() {
+    this.auth.signInWithRedirect(this.provider);
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import firebase from 'firebase';
 import './index.less';
+
+firebase.initializeApp({
+  apiKey: 'AIzaSyC0mhGKUKIERUXlB8Amh2kq9S6gjbiqg9A',
+  authDomain: 'camelot-ac734.firebaseapp.com',
+  databaseURL: 'https://camelot-ac734.firebaseio.com',
+  storageBucket: 'camelot-ac734.appspot.com',
+  messagingSenderId: '644309983634'
+});
+
+import App from './App';
 
 ReactDOM.render(
   <App />,
