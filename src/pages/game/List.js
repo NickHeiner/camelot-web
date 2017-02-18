5e858f79b3e6e346ad4956c5b192a970c252e15a
Nick Heiner <nth23@cornell.edu>
2017-02-18 00:41:56 -0500

    basic and meaningless firebase integration

--- src/pages/game/List.js
+++ src/pages/game/List.js
@@ -1,12 +1,39 @@
 import React, {PureComponent} from 'react';
 import { Button } from 'react-bootstrap';
+import autobind from 'autobind-decorator';
+import firebase from 'firebase';
 
 class GameList extends PureComponent {
+    constructor() {
+        super();
+        this.state = {count: null};
+    }
+    componentWillMount() {
+        this.database = firebase.database();
+
+        this.database.ref('widgets').on('value', snapshot => {
+            const val = snapshot.val();
+            if (val) {
+                this.setState(val);
+            }
+        });
+    }
+
     render() {
         return <div>
             <h1>Game List</h1>
-            <Button bsStyle="primary">New</Button>
+            <p>Count: {this.state.count}</p>
+            <Button bsStyle="primary" onClick={this.createNewGame}>New</Button>
         </div>;
+    }
+
+    @autobind
+    createNewGame() {
+        this.database.ref('widgets').set({count: this.state.count + 1}).then(() => {
+            debugger;
+        }, () => {
+            debugger;
+        });
     }
 }
 
