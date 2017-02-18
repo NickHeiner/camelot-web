60dff651573ea90a95b64dad79700d7d282ed86f
Nick Heiner <nth23@cornell.edu>
2017-02-18 00:23:45 -0500

    reading params right

--- src/pages/game/Play.js
+++ src/pages/game/Play.js
@@ -3,7 +3,7 @@ import React, {PureComponent} from 'react';
 class GamePlay extends PureComponent {
     render() {
         return <div>
-            <h1>GamePlay {this.props.route.id}</h1>
+            <h1>GamePlay {this.props.params.id}</h1>
         </div>;
     }
 }
