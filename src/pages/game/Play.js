8cc2aeae9b73ec56ce9a9d9fb8a39d405d99c791
Nick Heiner <nth23@cornell.edu>
2017-02-18 10:45:47 -0500

    building out the play view more

--- src/pages/game/Play.js
+++ src/pages/game/Play.js
@@ -1,10 +1,40 @@
 import React, {PureComponent} from 'react';
+import firebase from 'firebase';
+import autobind from 'autobind-decorator';
 
 class GamePlay extends PureComponent {
+    constructor() {
+        super();
+        this.state = {
+            game: undefined
+        };
+    }
+    componentWillMount() {
+        this.gameRef = firebase.database().ref(`games/${this.props.params.id}`);
+        this.gameRef.on('value', this.onGameUpdate);
+    }
+    
+    componentWillUnmount() {
+        this.gamesRef.off('value', this.onGameUpdate);
+    }
+
+    @autobind
+    onGameUpdate(snapshot) {
+        const val = snapshot.val();
+        this.setState({game: val});
+    }
+    
     render() {
-        return <div>
-            <h1>GamePlay {this.props.params.id}</h1>
-        </div>;
+        let gameDisplay;
+        if (this.state.game === undefined) {
+            gameDisplay = <p>Loading...</p>;
+        } else if (this.state.game === null) {
+            gameDisplay = <p>This link is not valid. Did someone share it with you incorrectly?</p>;
+        } else {
+            gameDisplay = <p>Host: {this.props.params.currentUser.displayName}</p>;
+        }
+
+        return gameDisplay;
     }
 }
 
