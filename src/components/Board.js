65850881711f0e3855eb39a47758507b25a8154a
Nick Heiner <nth23@cornell.edu>
2017-02-19 16:23:55 -0500

    Polishing the board appearance

--- src/components/Board.js
+++ src/components/Board.js
@@ -4,8 +4,10 @@ import camelotEngine from 'camelot-engine';
 import _ from 'lodash';
 import './Board.less';
 import autobind from 'autobind-decorator';
+import classnames from 'classnames';
 
-const {getBoardSpace, isValidMove} = camelotEngine().query();
+const {getBoardSpace, isValidMove, isGoal} = camelotEngine().query();
+const {applyMoves} = camelotEngine().update();
 
 class Board extends Component {
     render() {
@@ -16,47 +18,89 @@ class Board extends Component {
                     _(camelotConstants.BOARD_WIDTH)
                         .range()
                         .map(col => {
-                            const boardSpace = getBoardSpace(this.props.gameState, {row, col}),
-                                classNames = ['board-space'];
+                            const findBoardSpace = _.partial(getBoardSpace, this.props.gameState),
+                                boardSpace = findBoardSpace({row, col}),
+                                noTopBoardSpace = !findBoardSpace({row: row - 1, col}),
+                                noBottomBoardSpace = !findBoardSpace({row: row + 1, col}),
+                                noLeftBoardSpace = !findBoardSpace({row, col: col - 1}),
+                                noRightBoardSpace = !findBoardSpace({row, col: col + 1}),
+                                spaceClassNames = {
+                                    'board-space': true,
+                                    actual: boardSpace,
+                                    highlight: (row + col) % 2 === 1,
+                                    'first-row': row === 0 || noTopBoardSpace,
+                                    'last-row': row === camelotConstants.BOARD_HEIGHT - 1 || noBottomBoardSpace,
+                                    'first-col': col === 0 || noLeftBoardSpace,
+                                    'last-col': col === camelotConstants.BOARD_WIDTH - 1 || noRightBoardSpace
+                                };
 
-                            let glyph;
+                            let pieceIcon;
+
+                            const possibleValidMove = this.props.possibleMove.length && 
+                                isValidMove(
+                                    this.props.gameState, 
+                                    this.props.possibleMove.concat({row, col}), 
+                                    this.props.currentUserPlayer
+                                );
 
                             if (boardSpace) {
-                                classNames.push('actual');
-
+                                let glyph;
+                                const originalMoveBoardSpace = getBoardSpace(this.props.gameState, this.props.possibleMove[0]);
                                 if (boardSpace.piece) {
-                                    classNames.push(
-                                        'piece', 
-                                        boardSpace.piece.type,
-                                        boardSpace.piece.player === 'playerA' ? 'host' : 'opponent'
-                                    );
-                                    glyph = boardSpace.piece.type === 'pawn' ? 'pawn' : 'tower';
+                                    _.merge(spaceClassNames, {
+                                        [boardSpace.piece.type]: true,
+                                        host: boardSpace.piece.player === 'playerA',
+                                        opponent: boardSpace.piece.player === 'playerB',
+                                        'current-player': this.props.isCurrentUserActive && 
+                                            this.props.currentUserPlayer === boardSpace.piece.player
+                                    });
 
-                                    if (this.props.isCurrentUserActive && this.props.currentUserPlayer === boardSpace.piece.player) {
-                                        classNames.push('current-player');
+                                    const multiplePossibleMovesExist = this.props.possibleMove.length > 1;
+                                    if (multiplePossibleMovesExist) {
+                                        const gameStateIfPossibleMoveIsMade = 
+                                                applyMoves(this.props.gameState, this.props.possibleMove),
+                                            nextGameStateBoardSpace = 
+                                                getBoardSpace(gameStateIfPossibleMoveIsMade, {row, col});
+
+                                        spaceClassNames['will-be-captured'] = !nextGameStateBoardSpace.piece;
+                                    }
+
+                                    if (!(_.isEqual(originalMoveBoardSpace, boardSpace) && multiplePossibleMovesExist)) {
+                                        glyph = boardSpace.piece.type === 'pawn' ? 'pawn' : 'tower';
+                                    }
+                                } else {
+                                    const lastMoveBoardSpace = getBoardSpace(this.props.gameState, _.last(this.props.possibleMove));
+                                    if (possibleValidMove || _.isEqual(lastMoveBoardSpace, boardSpace)) {
+                                        glyph = originalMoveBoardSpace.piece.type === 'pawn' ? 'pawn' : 'tower';
+                                        _.merge(spaceClassNames, {
+                                            host: this.props.currentUserPlayer === 'playerA',
+                                            opponent: this.props.currentUserPlayer === 'playerB'
+                                        });
                                     }
                                 }
 
-                                if (_.find(this.props.possibleMove, {row, col})) {
-                                    classNames.push('possibly-moving-space');
+                                const isGoalSpace = isGoal(this.props.gameState, boardSpace.row, boardSpace.col);
+                                if (isGoalSpace && !glyph) {
+                                    glyph = 'star';
                                 }
 
-                                if (this.props.possibleMove.length && isValidMove(
-                                        this.props.gameState, 
-                                        this.props.possibleMove.concat({row, col}), 
-                                        this.props.currentUserPlayer
-                                    )
-                                ) {
-                                    classNames.push('possible-valid-move');
+                                if (glyph) {
+                                    pieceIcon = <Glyphicon glyph={glyph} />;
                                 }
+
+                                _.merge(spaceClassNames, {
+                                    'possibly-moving-space': _.find(this.props.possibleMove, {row, col}),
+                                    goal: isGoalSpace,
+                                    'possible-valid-move': possibleValidMove
+                                });
                             }
 
                             return (
                                 <div 
                                     key={`${row}-${col}`} 
-                                    onClick={() => this.onBoardSpaceClick(boardSpace)}
-                                    className={classNames.join(' ')}>
-                                    {glyph && <Glyphicon glyph={glyph} />}
+                                    onClick={() => this.onBoardSpaceClick(boardSpace, possibleValidMove)}
+                                    className={classnames(spaceClassNames)}>
+                                    {pieceIcon}
                                 </div>
                             );    
                         })
@@ -72,19 +116,14 @@ class Board extends Component {
     }
 
     @autobind
-    onBoardSpaceClick(boardSpace) {
-        if (!this.props.isCurrentUserActive) {
+    onBoardSpaceClick(boardSpace, possibleValidMove) {
+        if (!this.props.isCurrentUserActive || !boardSpace) {
             return;
         }
 
-        const possibleMoveAddition = _.pick(boardSpace, ['row', 'col']),
-            moveIsValidWithAddition = isValidMove(
-                this.props.gameState,
-                this.props.possibleMove.concat(possibleMoveAddition),
-                this.props.currentUserPlayer
-            );
+        const possibleMoveAddition = _.pick(boardSpace, ['row', 'col']);
 
-        if (!boardSpace.piece && this.props.possibleMove.length && moveIsValidWithAddition) {
+        if (!boardSpace.piece && possibleValidMove) {
             this.props.setPossibleMove(this.props.possibleMove.concat(possibleMoveAddition));
         } else if (this.props.currentUserPlayer === _.get(boardSpace, ['piece', 'player'])) {
             this.props.setPossibleMove([possibleMoveAddition]);
