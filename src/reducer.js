5b2ab4655df9136daea06c8d5727abfbb82448e9
Nick Heiner <nth23@cornell.edu>
2017-07-16 17:34:43 -0400

    Fixing behavior when you make moves and click on an invalid space

--- src/reducer.js
+++ src/reducer.js
@@ -1,29 +1,12 @@
 import * as Constants from './constants';
-import {fromJS, List} from 'immutable';
+import {fromJS} from 'immutable';
 import _ from 'lodash';
-import camelotEngine from 'camelot-engine';
-const {isValidMove} = camelotEngine().query();
+import {isValidMove} from './utils/camelot-engine';
+import {getCurrentGame, getCurrentUserPlayerName} from './utils/reducer-utils';
 
 const defaultState = fromJS({
   possibleMoveSteps: []
 });
-
-const getCurrentGame = (fullState, gameId) => fullState.firebase.getIn(['data', 'games', gameId]);
-
-const getCurrentUserPlayerName = (fullState, gameId) => {
-  const currentUserUid = fullState.firebase.getIn(['profile', 'uid']);
-  const currentGame = getCurrentGame(fullState, gameId);
-  const currentHostUid = fullState.firebase.getIn(['data', 'users', currentGame.get('host')]);
-  const currentOpponentUid = fullState.firebase.getIn(['data', 'users', currentGame.get('opponent')]);
-
-  if (currentUserUid === currentHostUid) {
-    return 'playerA';
-  }
-  if (currentUserUid === currentOpponentUid) {
-    return 'playerB';
-  }
-  return null;
-};
 
 export default (state = defaultState, action, restOfState) => {
   switch (action.type) {
@@ -37,7 +20,7 @@ export default (state = defaultState, action, restOfState) => {
 
       const stepsWithThisMoveAdded = possibleMoveSteps.push(fromJS(boardCoords));
       const possibleValidMove = isValidMove(
-        getCurrentGame(restOfState, action.payload.gameId),
+        getCurrentGame(restOfState, action.payload.gameId).get('gameState'),
         stepsWithThisMoveAdded, 
         getCurrentUserPlayerName(restOfState, action.payload.gameId)
       );
---
+++ src/utils/reducer-utils.test.js
@@ -0,0 +1,114 @@
+import {fromJS} from 'immutable';
+import * as ReducerUtils from './reducer-utils';
+
+describe('ReducerUtils', () => {
+  describe('getCurrentGame', () => {
+    it('gets the current game', () => {
+      const state = {
+        firebase: fromJS({
+          // eslint-disable-next-line id-blacklist
+          data: {
+            games: {
+              gameId: 'correct game',
+              otherGameId: 'wrong game'
+            }
+          }
+        })
+      };
+
+      expect(ReducerUtils.getCurrentGame(state, 'gameId')).toEqual('correct game');
+    });
+  });
+
+  describe('getCurrentUserPlayerName', () => {
+    it('handles the current user not being in the game', () => {
+      const state = {
+        firebase: fromJS({
+          profile: {
+            uid: 'currentPlayerUid'
+          },
+          // eslint-disable-next-line id-blacklist
+          data: {
+            games: {
+              gameId: {
+                host: 'hostId',
+                opponent: 'opponentId'
+              },
+              otherGameId: 'wrong game'
+            },
+            users: {
+              hostId: {
+                uid: 'somePlayerUid'
+              },
+              opponentId: {
+                uid: 'someOtherPlayerUid'
+              }
+            }
+          }
+        })
+      };
+
+      expect(ReducerUtils.getCurrentUserPlayerName(state, 'gameId')).toBeNull();
+    });
+
+    it('handles the current user being the host', () => {
+      const state = {
+        firebase: fromJS({
+          profile: {
+            uid: 'currentPlayerUid'
+          },
+          // eslint-disable-next-line id-blacklist
+          data: {
+            games: {
+              gameId: {
+                host: 'hostId',
+                opponent: 'opponentId'
+              },
+              otherGameId: 'wrong game'
+            },
+            users: {
+              hostId: {
+                uid: 'currentPlayerUid'
+              },
+              opponentId: {
+                uid: 'someOtherPlayerUid'
+              }
+            }
+          }
+        })
+      };
+
+      expect(ReducerUtils.getCurrentUserPlayerName(state, 'gameId')).toEqual('playerA');
+    });
+
+    it('handles the current user being the opponent', () => {
+      const state = {
+        firebase: fromJS({
+          profile: {
+            uid: 'currentPlayerUid'
+          },
+          // eslint-disable-next-line id-blacklist
+          data: {
+            games: {
+              gameId: {
+                host: 'hostId',
+                opponent: 'opponentId'
+              },
+              otherGameId: 'wrong game'
+            },
+            users: {
+              hostId: {
+                uid: 'someOtherPlayerUid'
+              },
+              opponentId: {
+                uid: 'currentPlayerUid'
+              }
+            }
+          }
+        })
+      };
+
+      expect(ReducerUtils.getCurrentUserPlayerName(state, 'gameId')).toEqual('playerB');
+    });
+  });
+});
