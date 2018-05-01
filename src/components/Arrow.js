eb3e7001fd7481cf4d70d256b6f41708b62a7ce2
Nick Heiner <nth23@cornell.edu>
2018-04-30 21:37:33 -0700

    I'd be lying if I said this looked good, but the basic functionality is there

--- src/components/Arrow.js
+++ src/components/Arrow.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+const arrowColor = 'black';
 const Arrow = ({width, height, style}) => <svg 
   width={width} height={height} {...style}
   viewBox="0.0 0.0 709.7165354330708 100.0" fill="none" stroke="none" 
@@ -11,9 +12,9 @@ const Arrow = ({width, height, style}) => <svg
   <g clipPath="url(#p.0)">
     <path fill="#000000" fillOpacity="0.0" d="m0 0l709.71655 0l0 100.0l-709.71655 0z" fillRule="evenodd"/>
     <path fill="#000000" fillOpacity="0.0" d="m12.582677 46.727036l691.937 -0.724411" fillRule="evenodd"/>
-    <path stroke="red" strokeWidth="12.0" 
+    <path stroke={arrowColor} strokeWidth="12.0" 
       strokeLinejoin="round" strokeLinecap="butt" d="m12.582677 46.727036l619.937 -0.6490326" fillRule="evenodd"/>
-    <path fill="red" stroke="red" strokeWidth="12.0" strokeLinecap="butt" 
+    <path fill={arrowColor} stroke={arrowColor} strokeWidth="12.0" strokeLinecap="butt" 
       d="m632.54047 65.89878l54.4364 -19.877789l-54.477905 -19.763767z" fillRule="evenodd"/>
   </g>
 </svg>;
--- src/components/MoveArrow.js
+++ src/components/MoveArrow.js
@@ -3,27 +3,28 @@ import Arrow from './Arrow';
 import {css} from 'glamor';
 import _ from 'lodash';
 
+const getRotationDegrees = (srcCoords, destCoords) => {
+  const rowDiff = srcCoords.row - destCoords.row; 
+  const colDiff = srcCoords.col - destCoords.col;
+
+  /* eslint-disable no-magic-numbers */
+  if (!colDiff) {
+    return rowDiff === -2 ? 90 : -90;
+  }
+  
+  if (!rowDiff) {
+    return colDiff === -2 ? 0 : 180;
+  }
+  
+  if (rowDiff === -2 && colDiff === -2) {
+    return 45;
+  }
+  /* eslint-enable no-magic-numbers */
+};
+
 export const getRotationAngle = (srcCoords, destCoords) => {
-  const getRotationValue = () => {
-    const rowDiff = srcCoords.row - destCoords.row; 
-    const colDiff = srcCoords.col - destCoords.col;
-
-    /* eslint-disable no-magic-numbers */
-    if (!colDiff) {
-      return rowDiff === -2 ? 90 : -90;
-    }
-    
-    if (!rowDiff) {
-      return colDiff === -2 ? 0 : 180;
-    }
-    
-    if (rowDiff === -2 && colDiff === -2) {
-      return 45;
-    }
-    /* eslint-enable no-magic-numbers */
-  };
-
-  return `rotate(${getRotationValue()}deg)`;
+  
+  return `rotate(${getRotationDegrees()}deg)`;
 };
 
 class MoveArrow extends PureComponent {
@@ -34,14 +35,21 @@ class MoveArrow extends PureComponent {
       return null;
     }
 
+    const rotationDegrees = getRotationDegrees(srcCoords, destCoords);
+    
+    const rightAngleFactorDeg = 90;
+    const arrowIsDiagonal = rotationDegrees % rightAngleFactorDeg;
+    const sideLength = arrowIsDiagonal
+      ? Math.sqrt(boardSpaceContentRect.bounds.width ** 2 + boardSpaceContentRect.bounds.width ** 2)
+      : boardSpaceContentRect.bounds.width;
+
     const style = css({
       position: 'absolute',
-      // transform: getRotationAngle(srcCoords, destCoords),
+      transform: `rotate(${rotationDegrees}deg) translateX(${sideLength}px)`,
       zIndex: 1
     });
 
-    // The arrow length calculation may not be correct if the arrow is going to be diagonal.
-    return <Arrow width={boardSpaceContentRect.bounds.width * 2} height={30} style={style} />;
+    return <Arrow width={sideLength * 2} height={30} style={style} />;
   };
 }
 
